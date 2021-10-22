2555309b7f004b2682b05087389827c59016ad1f
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-22 19:43:16 +0700

    Hibernate 1 to 1 unidirection

---
+++ src/com/luv2code/hibernate/demo/DeleteInstructorDetailDem.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDem {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure()
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+					
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+
+			session.beginTransaction();
+
+			
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, 3);
+			instructorDetail.getInstructor().setInstructorDetail(null);
+			
+			System.out.println(instructorDetail);
+		
+			if (instructorDetail != null) {
+				session.delete(instructorDetail);
+			}
+			
+			session.getTransaction().commit();
+			
+			
+				
+			System.out.println("Done");
+			
+		} catch (Exception exc)  {
+			exc.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+	
+}
